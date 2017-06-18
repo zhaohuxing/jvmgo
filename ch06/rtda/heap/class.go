bc6a300465b8b452092864cd227aec62af9bef0b
x_zhaohu <x_zhaohu@163.com>
2017-06-18 09:38:33 +0800

    class.go完成，但是需要依赖newObject(),newConstantPool(),newFields(),newMethods()

--- ch06/rtda/heap/class.go
+++ ch06/rtda/heap/class.go
@@ -47,8 +47,32 @@ func (self *Class) StaticVars() Slots {
 }
 
 //访问标志
+//修饰类的访问标志符如下: public final interface abstract super annotation enum synthetic
+func (self *Class) IsPublic() bool {
+	return self.accessFlags&ACC_PUBLIC != 0
+}
+func (self *Class) IsFinal() bool {
+	return self.accessFlags&ACC_FINAL != 0
+}
+func (self *Class) IsSuper() bool {
+	return self.accessFlags&ACC_SUPER != 0
+}
+func (self *Class) IsAbstract() bool {
+	return self.accessFlags&ACC_ABSTRACT != 0
+}
+func (self *Class) IsInterface() bool {
+	return self.accessFlags&ACC_INTERFACE != 0
+}
+func (self *Class) IsAnnotation() bool {
+	return self.accessFlags&ACC_ANNOTATION != 0
+}
+func (self *Class) IsEnum() bool {
+	return self.accessFlags&ACC_ENUM != 0
+}
+func (self *Class) IsSynthetic() bool {
+	return self.accessFlags & ACC_SYNTHETIC
+}
 
-//
 func (self *Class) isAccessibleTo(other *Class) bool {
 	return self.IsPublic() ||
 		self.getPackageName() == other.getPackageName()
@@ -60,4 +84,24 @@ func (self *Class) getPackageName() string {
 		return self.name[:i]
 	}
 	return ""
+}
+
+//get main method
+func (self *Class) GetMainMethod() *Method {
+	return self.getStaticMethod("main", "([Ljava/lang/String;)V")
+}
+func (self *Class) getStaticMethod(name, descriptor string) *Method {
+	for _, method := range self.methods {
+		if method.IsStatic() &&
+			method.name == name &&
+			method.descriptor == descriptor {
+			return method
+		}
+	}
+	return nil
+}
+
+//NewOject
+func (self *Class) NewObject() *Object {
+	return newObject(self) // newObject(self) 方法位于heap/object.go中
 }
